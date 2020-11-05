7821cc14e775a6bb75dd22c3f358f962ca091bad
adelinedepaepe <adeline.depaepe@gmail.com>
2020-11-05 17:46:00 +0100

    ajout commentaires sur exercices python

--- 3-Python/Ex_cours.py
+++ 3-Python/Ex_cours.py
@@ -1,16 +1,17 @@
+
 mois =['Janvier','Février','Mars','Avril','Mai','Juin','Juillet']
 month =mois.copy()
 
-month.sort()
+month.sort() #va agir sur la liste, la liste va être inversée
 print(month)
 
-print(sorted(mois))
+print(sorted(mois)) # On affiche la liste inversée, mais la liste va garder son ordre d'origine
 
 month.reverse()
 print(month)
 
 print(reversed(mois))
-print(list(reversed(mois)))
+print(list(reversed(mois))) # je ne sais pas pourquoi, mais contrairement à sorted, il faut mettre liste en plus.. voir 'iterator object'
 
 
 
