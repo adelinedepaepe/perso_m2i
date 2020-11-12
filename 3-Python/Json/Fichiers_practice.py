ecce1796d3d0385715975d1c85c1d39db8d0dc89
adelinedepaepe <adeline.depaepe@gmail.com>
2020-11-12 15:38:21 +0100

    paractice fichiers json

--- 3-Python/Json/Fichiers_practice.py
+++ 3-Python/Json/Fichiers_practice.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import json
 
-with open('../../../Yassine/m2i/data/hr/employees.csv') as f:
+with open( '../../../../Yassine/m2i/data/hr/employees.csv' ) as f:
   employees = [ ##Ici on crée une liste de dictionnaires avec le '[]'
       {
         'FIRST_NAME':line.split(';')[1],
@@ -11,7 +11,7 @@ with open('../../../Yassine/m2i/data/hr/employees.csv') as f:
         for line in f if 'EMPLOYEE_ID' not in line ]
 
 
-with open('../../../Yassine/m2i/data/hr/departments.csv') as f:
+with open( '../../../../Yassine/m2i/data/hr/departments.csv' ) as f:
    departments ={ ##En ne mettant pas les crochets, on créé un dictionnaore simple, et non une liste de dictionnaires
             line[1:].split(';')[0]:line.split ( ';' )[1] #On ne s'intéresse qu'aux valeurs
        for line in f if 'DEPARTMENT_ID' not in line} #Le if permet de supprimer la ligne des en-tetes
@@ -23,7 +23,7 @@ print(departments)
 my_employees=[{'FIRST_NAME':e['FIRST_NAME'], 'LAST_NAME': e['LAST_NAME'], 'DEPARTMENT_NAME':departments.get(e['DEPARTMENT_ID'],0)} for e in employees]
 
 #J'écris mon résultat dans un fichier json. Il faut en amont importer json en haut du fichier 'import json'
-with open('my_employees.jsonl', 'w') as f:
+with open( 'my_employees.jsonl', 'w' ) as f:
   json.dump(my_employees, f)
 
 
